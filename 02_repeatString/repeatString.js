224792d77e9ec640a4903c3313b6e89f5e4255cf
khoo02 <khooiverson02@gmail.com>
2024-08-18 05:10:20 -0700

    complete function; add loop and error check

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, numOfTimes) {
+    // check negative number
+    if (numOfTimes < 0) {
+        return "ERROR";
+    }
+    // use concatenateString to add word numOfTimes
+    let concatenateString = "";
+    for (let i = 0; i < numOfTimes; i++) {
+        concatenateString += word;
+    }
+    return concatenateString;
 };
 
 // Do not edit below this line
