3d08e6575c943e9181eee84cf48cfbc6d0daa47b
khoo02 <khooiverson02@gmail.com>
2024-08-20 10:03:15 -0700

    create loop to finish reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    // new string to store reversed text
+    let reverseText = "";
 
+    // iterate string index backwards
+    for (let i = text.length - 1; i >= 0; i--) {
+        // add last elements first
+        reverseText += text[i];
+    }
+
+    return reverseText;
 };
 
 // Do not edit below this line
