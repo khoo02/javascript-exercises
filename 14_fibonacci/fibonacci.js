621a3008883b20819cf75099ed3976b9914ddef2
khoo02 <khooiverson02@gmail.com>
2025-06-06 17:04:36 -0700

    remove old copies

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // check if string and convert to integer
+    num = Number(num);
+    
+    // check if zero
+    if (num === 0) {
+        return 0;
+    }
+    if (num === 1) {
+        return 1;
+    }
 
+    // starting numbers
+    let prev = 0, curr = 1;
+    let result = 0;
+
+    // calculate fibonacci number
+    for (let i = 2; i <= num; i++) {
+        // add current and previous number
+        result = prev + curr;
+
+        // update prev to curr, and curr to next
+        prev = curr;
+        curr = result;
+    }
+
+    // return result
+    return result;
 };
 
 // Do not edit below this line
